e6cb42897a60531a20bc986be61d90a41df6c5cc
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-07-30 18:41:26 -0700

    Add project files.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(30);
+            tree.left = new Node(23);
+            tree.right = new Node(35);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BinarySearchTree/Search.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Search
+    {
+        public bool BinarySearchTreeSearch(Node root, int item)
+        {
+            if (root == null)
+                return false;
+            if (root.data == item)
+                return true;
+            else if (root.data < item)
+                BinarySearchTreeSearch(root.left, item);
+            else
+                BinarySearchTreeSearch(root.right, item);
+
+            return false;
+        }
+    }
+}
